5567ae2ebba1ec77ee95a64e001e197fa67a036f
Ivan Frondozo <ivan-p.frondozo@gcash.com>
2025-08-13 23:08:16 +0800

    Added all main and utility files to execute program exhibiting inheritance and interfaces

---
+++ src/main/java/org/example/AdvancedRecon.java
@@ -0,0 +1,8 @@
+package org.example;
+
+// File: AdvancedRecon.java
+// This interface extends two other interfaces!
+public interface AdvancedRecon extends VisualRecon, SignalIntel {
+    // This interface is a combination of two contracts.
+    // It doesn't need a body, it just aggregates other interfaces.
+}
---
+++ src/main/java/org/example/QuadCopter.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class QuadCopter extends Drone implements VisualRecon {
+
+    public QuadCopter(String callsign) {
+        super(callsign); // Call the parent constructor
+    }
+
+    // Implementing the abstract method from the Drone class
+    @Override
+    public void fly() {
+        System.out.println(getCallsign() + " is hovering with four rotors.");
+    }
+
+    // Implementing the abstract method from the VisualRecon interface
+    @Override
+    public void takePicture() {
+        System.out.println(getCallsign() + " takes a picture with its " + SENSOR_TYPE);
+    }
+}
